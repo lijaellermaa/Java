efccb4f8d41eaaecdb112c3daeb21bc14ff33956
lijaellermaa <lija5335@gmail.com>
2022-05-04 23:11:24 +0300

    new commit

--- JavaOmis/src/com/company/MainArraysFor.java
+++ JavaOmis/src/com/company/MainArraysFor.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MainArraysFor {
     public static void main(String[] args) {
-        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
         for (int i = 0; i < array.length; i++) {
             array[i] = 0;
         }
